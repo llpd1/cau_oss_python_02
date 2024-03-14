6c26f1aa54a41e71d392518bf0541900540bff27
llpd1 <163384478+llpd1@users.noreply.github.com>
2024-03-14 16:18:03 +0900

    Update volume_calc.py
    1주차 부피 계산 작성

--- volume_calc.py
+++ volume_calc.py
@@ -1,1 +1,9 @@
+width=float(input())
+print("가로: ", width)
+depth=float(input())
+print("세로: ", depth)
+heigh=float(input())
+print("높이: ", heigh)
 
+volume=width*depth*heigh
+print("박스의 부피는 %d입니다.", volume)
